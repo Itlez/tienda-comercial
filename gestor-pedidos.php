03f0c156ac44d2b1c8d2bb84a5f8047da939b229
Itlez <leo.garcias@gmail.com>
2026-07-27 22:34:18 -0400

    Update gestor-pedidos.php

--- gestor-pedidos.php
+++ gestor-pedidos.php
@@ -1,1 +1,22 @@
+#Formulario
 
+<H1>formulario Compras</H1>
+    <form action="GuardarCompra.php" method="POST">
+        <label>Idcliente:</label><br>
+        <input type="number" name="id_cliente"><br><br>
+
+        <label>total:</label><br>
+        <input type="number" name="total"><br><br>   
+
+        <label>cantidad:</label><br>
+        <input type="number" name="cantidad"><br><br>
+
+        <label>fecha:</label><br>
+        <input type="date" name="fecha"><br><br>
+
+        <label>Idproducto:</label><br>
+        <input type="number" name="id_producto"><br><br>
+
+        <input type="submit" value="Guardar">
+
+    </form>
