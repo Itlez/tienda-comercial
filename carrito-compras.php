34f801bb66e49255c7256607b1f7c629f4e97ef9
Itlez <leo.garcias@gmail.com>
2026-07-27 22:04:52 -0400

    Update carrito-compras.php
    revisar codigo.

--- carrito-compras.php
+++ carrito-compras.php
@@ -1,1 +1,28 @@
 Visto desde: lgarc989
+
+codigo:
+<?php
+
+include("conexion.php");
+
+$nombre=$_POST["nombre"];
+$descripcion=$_POST["descripcion"];
+$precio=$_POST["precio"];
+$stock=$_POST["stock"];
+
+$sql="INSERT INTO PRODUCTO(nombre,descripcion,precio,stock)
+VALUES('$nombre','$descripcion','$precio','$stock')";
+
+if($conn->query($sql)==TRUE){
+
+echo "Producto registrado correctamente.";
+
+}else{
+
+echo "Error ".$conn->error;
+
+}
+
+$conn->close();
+
+?>
